93ce40736aa6b3044e25ed4548805adcc7a818ce
ChimpOnCloud <98509703+ChimpOnCloud@users.noreply.github.com>
2022-02-26 16:58:43 +0800

    Add files via upload
    first demo

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int hello = 3;
+    int world  = 4;
+    int helloWorld = hello + world;
+    printf("%d\n", helloWorld);
+    return 0;
+}
